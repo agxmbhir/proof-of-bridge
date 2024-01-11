2cbaf67ae622bc5380f558e7aa46a7801682d7fb
agxmbhir <agambhir5656@gmail.com>
2024-01-12 03:03:52 +0530

    fix event

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,33 +1,29 @@
 //@ts-ignore
-import { Block, require } from "@hyperoracle/zkgraph-lib";
+import { Block, ByteArray, require } from "@hyperoracle/zkgraph-lib";
 import { Bytes, Event, BigInt } from "@hyperoracle/zkgraph-lib";
 
-const esigBurn = "0xcc16f5dbb4873280815c1ee09dbd06736cffcc184412cf7a71a0fdb75d397ca5";
-const esigBurnU32 = BigInt.fromString(esigBurn.slice(0, 8)).toU32();
+const esigBurn = Bytes.fromHexString("0x42966c689b5afe9b9b3f8a7103b2a19980d59629bfd6a20a60972312ed41d836");
+const zeroAddress = Bytes.fromHexString('0x0000000000000000000000000000000000000000000000000000000000000000')
+
 
 let mintIdentifier = "6bc63893";
 let mintIdentifierBytes = Bytes.fromHexString(mintIdentifier).padEnd(32);
 
 export function handleBlocks(blocks: Block[]): Bytes {
   let events = blocks[0].events;
-  let burnAmount: Bytes = Bytes.fromHexString("0x"); // User's amount;
-  let burnAddress: Bytes = Bytes.fromHexString("0x"); // User's address;
-  let noEvent = true;
-
-  for (let i = events.length - 1; i >= 0; i--) {
-    if (events[i].esig.toU32() == esigBurnU32) {
-      //   console.log('SYNC event');
-      burnAddress = events[i].data.slice(0, 32);
-      burnAmount = events[i].data.slice(32, 64);
-      noEvent = false;
+  let burnEvent: Event | null = null;
+  for (let i = 0; i < events.length; i++) {
+    if (events[i].topic2 == zeroAddress) {
+      burnEvent = events[i]
       break;
     }
   }
-  if (noEvent) {
-    require(false);
-    return Bytes.empty(); // Omit compile error, never goes here
-  } else {
-    let payload = burnAddress.concat(burnAmount);
-    return Bytes.fromByteArray(mintIdentifierBytes.concat(payload));
+  let payload: ByteArray = new ByteArray(0); // Initialize payload as an empty array
+  if (burnEvent) {
+    let burnAddress = burnEvent.topic1;
+    let burnAmount = burnEvent.data;
+    payload = burnAddress.concat(burnAmount);
   }
+  return Bytes.fromByteArray(mintIdentifierBytes.concat(Bytes.fromByteArray(payload)));
 }
+
